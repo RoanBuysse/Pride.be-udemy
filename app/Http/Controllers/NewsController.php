76005b83d4ed5d2b669ad0c879921ac43a2e2c5e
RoanBuysse <roan.buysse@telenet.be>
2018-03-23 15:41:49 +0100

    fixed

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -1,20 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\News;
 use App\NewsCategory;
-
-
 class NewsController extends Controller
 {
-
     public function __construct()
     {
       $this->middleware('isWriter',['only' => ['create','edit']]);
     }
-
     public function index()
     {
         $news = News::all();
@@ -22,15 +16,12 @@ class NewsController extends Controller
         // dd($news);
         // dd($newstranslation);
     }
-
     public function create()
-    {
-        if(LaravelLocalization::getCurrentLocale()=='nl'){ $category = NewsCategory::pluck('nameNl','id');}
-        if(LaravelLocalization::getCurrentLocale()=='en'){ $category = NewsCategory::pluck('nameEn','id');}
-        if(LaravelLocalization::getCurrentLocale()=='fr'){ $category = NewsCategory::pluck('nameFr','id');}
-        return view('news.create', compact('news_categories'));
+    {  
+    $news_categories  = NewsCategory::pluck('nameNl','id');
+    return view('news.create', compact('news_categories'));
     }
-
+    
     public function store(Request $request)
     {
        $input = $request->all();
@@ -41,22 +32,17 @@ class NewsController extends Controller
        }
         return back();
     }
-
     public function show($id)
     {
         $news = News::findOrFail($id);
         // var_dump($news);
         return view('news.show', compact('news'));
-
     }
-
     public function edit($id)
     {
         $news = News::findOrFail($id);
         return view('news.edit', compact('news'));
     }
-
-
     public function update(Request $request, $id)
     {
         $input = $request->all();
@@ -65,12 +51,10 @@ class NewsController extends Controller
         return back();
         
     }
-
     public function destroy(Request $request, $id)
     {
         $news = News::findOrFail($id);
         $news->delete($request->all());
         return redirect('/news');
     }
-
 }
