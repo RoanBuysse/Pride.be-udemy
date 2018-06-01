37a2ed5ccff6c3f98a24616008f89e6da2b6495e
RoanBuysse <roan.buysse@telenet.be>
2018-06-01 19:05:10 +0200

    fixed iamge

--- app/Http/Resources/News.php
+++ app/Http/Resources/News.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
-class News extends Resource
+class News extends JsonResource
 {
      /**
      * Transform the resource into an array.
--- resources/assets/js/components/News/News.vue
+++ resources/assets/js/components/News/News.vue
@@ -2,8 +2,11 @@
    <article>
                    <div class="card card-body" v-for="news in news" v-bind:key="news.id">
                    <h3>{{news.title}}</h3>
+                    <p v-for="photo in photo" v-bind:key="photo" v-if="news.photo_id === photo.id">{{photo.photo}}</p>
                    <p>{{news.body}}</p>
+                   <!-- <p>{{photo.photo}}</p> -->
                    </div>
+                   
 
    </article>
 </template>
@@ -13,6 +16,7 @@
        data(){
         return {
             news: [],
+            photo: [],
             news: {
                 id: '',
                 title:'',
@@ -25,13 +29,24 @@
                 // bodyEn:'',
                 photo_id:''
             },
+
+            photo: {
+                id: '',
+                photo: ''
+            },
             news_id:'',
+            photo_id:'',
             paginaton: {},
             edit: false
             };
+            
        },
       created() {
           this.fetchNews();
+          this.fetchPhotos();
+        //   this.idtoLink();
+            
+            
       },
  
       methods: {
@@ -40,9 +55,33 @@
               .then(res => res.json())
               .then(res => {
                this.news = res.data;
-               console.log(this.news);
+                
+            //    console.log(this.news);
             });
-          }
+          },
+
+          fetchPhotos() {
+              fetch('api/photo')
+              .then(res => res.json())
+              .then(res => {
+               this.photo = res.data;
+              
+               
+            
+            });
+             this.editNews();
+            //  console.log(photo);
+          },
+
+        editNews(news, photo) 
+        {
+        //   this.news=Vue.set(this.news, 2, {photo_id: '456', body: 'file2-b'});
+        //  this.news=Vue.set(this.news, this.news.findIndex(f => f.token === '1'), {token: '456', name: 'file2-b'})
+        //   console.log(this.photo);
+        // Route::get('/api/questions', function() { return Questions::with('User')->get(); } );
+        }
+
+         
       }
       
 
